307a506988182c282a4f99d3ad2dbb478ae4ff74
Felipe Pereira <126419915+felipereira10@users.noreply.github.com>
2025-11-04 18:11:52 -0300

    feat. cards test

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { GoIssueClosed } from "react-icons/go";
+import RecipeReviewCard from "./RecipeReviewCard";
 
 function AboutCard() {
   return (
+    <>
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
@@ -14,8 +16,8 @@ function AboutCard() {
             I’m currently working as a{" "}
             <span className="blue">Intern Software Developer</span> at{" "}
             <span className="blue">Redenção Turismo</span>.
-            <br />I hold two Graduations in{" "}
-            <span className="blue">Análise e Desenvolvimento de Sistemas</span> and <span className="blue">Logística</span> from{" "}
+            <br />I hold two Degrees in{" "}
+            <span className="blue">Systems Analysis and Development</span> and <span className="blue">Logistics</span> from{" "}
             <span className="blue">FATEC São José dos Campos</span>.
             <br />
             <br />
@@ -25,16 +27,16 @@ function AboutCard() {
 
           <ul>
             <li className="about-activity">
-              <span className="blue"> <GoIssueClosed /> </span> I love games, because it helps me in logical thinking, I met good stories and made great friends. The MVP is Red Dead Redemption 2!!!
+              <span className="blue"> <GoIssueClosed /> </span> I love games — they improve my logical thinking, introduced me to amazing stories, and helped me make great friends. The GOAT is Red Dead Redemption 2!!!
             </li>
             <li className="about-activity">
-              <span className="blue"> <GoIssueClosed /> </span> I practice Muay Thai and Brazilian Jiu Jitsu, from where I bring discipline and respect! My idol is the Poatan
+              <span className="blue"> <GoIssueClosed /> </span> I practice Muay Thai and Brazilian Jiu Jitsu, which taught me discipline and respect. My idol is the Poatan
             </li>
             <li className="about-activity">
               <span className="blue"> <GoIssueClosed /> </span> I love anime and manga, because I have been taught a lot since I was a kid! (Example: One Piece, Hunter x Hunter, Death Note, Nanatsu no Taizai and many others too)
             </li>
             <li className="about-activity">
-              <span className="blue"> <GoIssueClosed /> </span> I am passionate about traveling and exploring new places, it is curious to know new cultures and customs. Cause I like talking a lot
+              <span className="blue"> <GoIssueClosed /> </span>I'm passionate about traveling and discovering new cultures — I love talking to people and learning their customs.
             </li>
             <li className="about-activity">
               <span className="blue"> <GoIssueClosed /> </span> The gym keeps me physically and mentally strong — it has always helped me a lot.
@@ -43,17 +45,19 @@ function AboutCard() {
               <span className="blue"> <GoIssueClosed /> </span> I love listening to music, I basically have it on all day. My taste is mixed — classical music, pagode, trap, and more. My favorite song is “Tá Escrito” by Grupo Revelação.
             </li>
             <li className="about-activity">
-              <span className="blue"> <GoIssueClosed /> </span> And of course, I have my heart team. Corinthians since I was born, thanks to my hero, MY DAD!
+              <span className="blue"> <GoIssueClosed /> </span> And of course, I have my team. Corinthians since I was born — thanks to my hero, MY DAD!
             </li>
           </ul>
 
           <p style={{ color: "#91d2ddff" }}>
-            "I've always been a dreamer, that's what keeps me alive!"{" "}
+            "I've always been a dreamer, that's what keeps me alive!"
           </p>
           <footer className="blockquote-footer">Racionais MC's</footer>
         </blockquote>
       </Card.Body>
+      <RecipeReviewCard />
     </Card>
+    </>
   );
 }
 
